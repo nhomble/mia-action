39bdb81561ad5be13fa3412c013b19169795ca04
Nicolas Homble <nhomble@terpmail.umd.edu>
2021-09-14 00:01:17 -0400

    init mia feature
    - break on git log being too old
    - break on no commits ever

---
+++ entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/sh -l
+
+AUTHOR="$1"
+DAYS="$2"
+
+# 2021-08-20
+LAST_COMMIT_DATE="$(git log --author="$AUTHOR" -1 --pretty=%ad --date=short)"
+NOW="$(date +%F)"
+
+printf "lastCommitDate=%s now=%s\n" "$LAST_COMMIT_DATE" "$NOW"
+
+if [ -z "$LAST_COMMIT_DATE" ]; then
+  echo "Nothing happened!"
+  exit 1
+fi
+
+LAST_EPOCH=$(date --date="$LAST_COMMIT_DATE" +%s)
+NOW_EPOCH=$(date --date="$NOW" +%s)
+
+printf "lastEpoch=%s now=%s\n" "$LAST_EPOCH" "$NOW_EPOCH"
+
+EPOCH_DIFF=$(($NOW_EPOCH - $LAST_EPOCH))
+DAY_DIFF=$(($EPOCH_DIFF / (60 * 60 * 24)))
+
+printf "epochDiff=%s dayDiff=%s\n" "$EPOCH_DIFF" "$DAY_DIFF"
+
+if [ $DAY_DIFF -gt $DAYS ]; then
+  echo "It's been too many days"
+  exit 1
+fi
