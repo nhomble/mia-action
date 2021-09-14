e13adfd6bae660517695eb18b3ff76641839ad35
nhomble <nhomble@terpmail.umd.edu>
2021-09-13 23:51:21 -0400

    check for empty

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,22 @@ DAYS="$2"
 LAST_COMMIT_DATE="$(git log --author="$AUTHOR" -1 --pretty=%ad --date=short)"
 NOW="$(date +%F)"
 
+printf "lastCommitDate=%s now=%s\n" "$LAST_COMMIT_DATE" "$NOW"
+
+if [ "" -eq "$LAST_COMMIT_DATE" ]; then
+  echo "Nothing happened!"
+  exit 1
+fi
+
 LAST_EPOCH=$(date --date="$LAST_COMMIT_DATE" +%s)
 NOW_EPOCH=$(date --date="$NOW" +%s)
 
+printf "lastEpoch=%s now=%s\n" "$LAST_EPOCH" "$NOW_EPOCH"
+
 EPOCH_DIFF=$(($NOW_EPOCH - $LAST_EPOCH))
 DAY_DIFF=$(($EPOCH_DIFF / (60 * 60 * 24)))
+
+print "epochDiff=%s dayDiff=%s\n" "$EPOCH_DIFF" "$DAY_DIFF"
 
 if [ $DAY_DIFF -gt $DAYS ]; then
   echo "It's been too many days"
